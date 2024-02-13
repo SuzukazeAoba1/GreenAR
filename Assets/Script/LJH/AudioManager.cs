3af7a5cfd5d619bc2911d12486f4989236ae337a
DESKTOP-K22\G <noskang6699@naver.com>
2024-02-13 15:47:56 +0900

    인트로씬 추가, BGM  재생 시스템 추가, 폰트 추가

---
+++ Assets/Script/LJH/AudioManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyAudioPlayer : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip myclip;
+    public AudioClip myclip2;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        // AudioSource가 끝났을 때 실행될 콜백 함수 지정
+        audioSource.loop = false;
+        audioSource.playOnAwake = false;
+        audioSource.clip = myclip;
+        audioSource.Play();
+        StartCoroutine(WaitForAudioToEnd());
+    }
+
+    IEnumerator WaitForAudioToEnd()
+    {
+        // 오디오가 재생 중인 동안 대기
+        yield return new WaitWhile(() => audioSource.isPlaying);
+
+        // 오디오가 끝났을 때 실행할 코드
+        //Debug.Log("Audio has finished playing");
+        audioSource.clip = myclip2;
+        audioSource.loop = true;
+        audioSource.Play();
+        // 여기에 실행할 함수를 넣으면 됩니다.
+    }
+}
